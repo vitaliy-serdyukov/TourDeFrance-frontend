0e040c8db9997e5ad96a157d91c32230e65ddd11
vital <vitaliy_serdyukov@hotmail.com>
2022-05-31 07:45:10 +0200

    sorting methods added

--- js/rider/fetch-rider.js
+++ js/rider/fetch-rider.js
@@ -13,23 +13,55 @@ function fetchAllRiders() {
 async function createRiderMap() {
   /*  out("show all riders");*/
   const riderList = await fetchAllRiders();
-  const sortedList = sortListByRiderTime(riderList);
+  const sortedList = sortByRiderTime(riderList);
   out(riderList);
   sortedList.forEach((rider) => {
     riderMap.set(rider.riderId, rider);
   })
+
+
+  getQuckestRider(sortedList);
+  const mPoints = getWhiteTshirt(sortedList);
+  out(mPoints);
+
+
+  getWhiteTshirt(riderList);
 }
 
 
 
-// sorting our list with riders by rider time in descending (reverse) order TODO doesn't work 100 procent
-function sortListByRiderTime(riderList) {
+// sorting our list with riders by rider time in ascending order
+function sortByRiderTime(riderList) {
   return riderList.sort((a, b) => {
-    return a.riderTime - b.riderTime
+    return a.riderTime - b.riderTime;
   });
 }
 
-/*function getQuckestRider(sort){
-  sortListByRiderTime(riderList)
-  return
-}*/
+function getQuckestRider(timeSort){ // a yellow t-shirt
+  sortByRiderTime(timeSort);
+  out(timeSort[0]);
+  return timeSort[0];
+}
+
+function sortByMountainPoints(riderList) {
+  return riderList.sort((a, b) => {
+    return a.mountainPoints - b.mountainPoints;
+  });
+}
+
+function sortBySprintPoints(riderList) {
+  return riderList.sort((a, b) => {
+    return a.sprintPoints - b.sprintPoints;
+  });
+}
+
+function getWhiteTshirt(riderList) {
+  const timeSort = riderList.sort((a, b) => {
+    return a.riderTime - b.riderTime;
+  });
+  if(timeSort[0].age < 26){
+    out('White T-shirt '+  timeSort[0].firstName + ' ' + timeSort[0].lastName);
+    return timeSort[0];
+  }
+  return out('Nobody should have the white T-shirt');
+}
