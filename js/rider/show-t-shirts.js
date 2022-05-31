47db5b0ddf2bc6a143782998660a60f1933c3a9f
vital <vitaliy_serdyukov@hotmail.com>
2022-05-31 10:53:42 +0200

    last commit

--- js/rider/show-t-shirts.js
+++ js/rider/show-t-shirts.js
@@ -1,11 +1,14 @@
-out('TSHIRTS');
+/**
+ *  place on index to print 4 t-shirt owners
+ *  @author Vitaliy
+ */
 
 const tShirtsTable = document.getElementById("tShirtsTable");
 document.addEventListener('DOMContentLoaded', addRow);
 
 function addRow() {
 
-  const sortList = createRiderMap();
+ /* const sortList = createRiderMap();*/
 
   let row = tShirtsTable.insertRow(3);
 
@@ -13,10 +16,6 @@ function addRow() {
   let cell2 = row.insertCell(1);
   let cell3 = row.insertCell(2);
   let cell4 = row.insertCell(3);
-
-
-  const c1 = getYellowTshit(sortList);
-  out(c1);
 
   cell1.innerText = "1";
   cell2.innerText = "2";
